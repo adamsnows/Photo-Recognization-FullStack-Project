0aa09f80ab3f870fae4eb4f4d0c74e7510527333
Adam Neves <adaamangelow@gmail.com>
2025-04-12 23:57:24 -0300

    wip: trying to insert photos on database

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,8 @@
 import Fastify from "fastify";
 import {
-	type ZodTypeProvider,
-	serializerCompiler,
-	validatorCompiler,
+  type ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
 } from "fastify-type-provider-zod";
 
 import { photosRoutes } from "./photos/routes";
@@ -12,4 +12,16 @@ export const app = Fastify().withTypeProvider<ZodTypeProvider>();
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-await app.register(photosRoutes, { prefix: "/photos" });
+async function startApp() {
+  await app.register(photosRoutes, { prefix: "/photos" });
+  try {
+	await app.listen({ port: 3333, host: "0.0.0.0" });
+	console.log("Server running at http://0.0.0.0:3333");
+	
+  } catch (err) {
+    console.error("Error starting server", err);
+    process.exit(1);
+  }
+}
+
+startApp();
