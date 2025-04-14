a47bec8a5981d470fd99cca90bb8453ff0777adb
Adam Neves <adaamangelow@gmail.com>
2025-04-14 13:15:16 -0300

    fix: placeholder when no images

--- frontend/src/components/gallery/index.jsx
+++ frontend/src/components/gallery/index.jsx
@@ -7,33 +7,43 @@ import Skeleton from "react-loading-skeleton";
 const GalleryGrid = () => {
   const { photos, loading } = useSearch();
 
+  if (loading) {
+    return (
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-[20px]">
+        {Array(10)
+          .fill(0)
+          .map((_, index) => (
+            <div
+              key={index}
+              className="relative h-[320px] w-[193px] rounded-[18px]"
+            >
+              <Skeleton height="100%" width="100%" className="rounded-[18px]" />
+            </div>
+          ))}
+      </div>
+    );
+  }
+
+  if (photos.length === 0) {
+    return (
+      <div className="flex text-center">
+        <span className="text-gray-600 min-h-[400px]">
+          Nenhuma imagem disponível de acordo com sua pesquisa
+        </span>
+      </div>
+    );
+  }
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-[20px]">
-      {loading
-        ? Array(10)
-            .fill(0)
-            .map((_, index) => (
-              <div
-                key={index}
-                className="relative h-[320px] w-[193px]  rounded-[18px]"
-              >
-                <Skeleton
-                  height="100%"
-                  width="100%"
-                  className="rounded-[18px]"
-                />
-              </div>
-            ))
-        : photos
-            .slice(0, 10)
-            .map((photo) => (
-              <GalleryCard
-                key={photo.id}
-                url={photo.imageUrl}
-                collection={photo.collection}
-                {...photo}
-              />
-            ))}
+      {photos.slice(0, 10).map((photo) => (
+        <GalleryCard
+          key={photo.id}
+          url={photo.imageUrl}
+          collection={photo.collection}
+          {...photo}
+        />
+      ))}
     </div>
   );
 };
