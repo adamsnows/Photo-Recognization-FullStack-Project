84bf5e4207ca2be8dbdd468c71df8d223469b755
Adam Neves <adaamangelow@gmail.com>
2025-04-14 01:14:28 -0300

    fix: removing console.logs

--- backend/src/photos/routes.ts
+++ backend/src/photos/routes.ts
@@ -12,7 +12,6 @@ interface Params {
 
 export async function photosRoutes(fastify: FastifyInstance) {
   fastify.get("/photos", async (_req, reply) => {
-    console.log("📥 Requisição GET /photos recebida");
     const photos = await getPhotos();
     return reply.send(photos);
   });
@@ -27,7 +26,7 @@ export async function photosRoutes(fastify: FastifyInstance) {
         }>,
         reply
       ) => {
-        console.log("📥 Requisição GET /search recebida com termo:", request.query.term);
+
         const { term } = request.query;
         const photos = await searchPhotos(term);
         return reply.send(photos);
@@ -36,8 +35,6 @@ export async function photosRoutes(fastify: FastifyInstance) {
 
   fastify.get("/images/:id", async (req, reply) => {
     const { id } = req.params as { id: string };
-    console.log(`📥 Requisição GET /images/${id} recebida`);
-
     const photo = await prisma.photo.findUnique({
       where: {
         id: Number(id),
@@ -55,36 +52,23 @@ export async function photosRoutes(fastify: FastifyInstance) {
   });
 
   fastify.post("/search-by-image", async (req, reply) => {
-    console.log("📥 Requisição POST /search-by-image recebida");
-    console.log("Content-Type:", req.headers["content-type"]);
-  
     try {
       const parts = req.parts();
       let imageFound = false;
   
       for await (const part of parts) {
         if (part.type === "file") {
-          console.log("🧩 Parte recebida (arquivo):", {
-            fieldname: part.fieldname,
-            filename: (part as any).filename,
-            mimetype: (part as any).mimetype,
-          });
-  
           if (part.fieldname === "image") {
             imageFound = true;
             const imageBuffer = await part.toBuffer();
-            console.log("🖼 Buffer de imagem recebido, tamanho:", imageBuffer.length);
   
             const result = await searchByImage(imageBuffer);
             return reply.send(result);
           }
-        } else {
-          console.log("📄 Parte recebida (campo):", part.fieldname);
-        }
+        } 
       }
   
       if (!imageFound) {
-        console.warn("🚫 Nenhuma parte com fieldname 'image' foi recebida");
         return reply.status(400).send({ message: "Imagem não fornecida" });
       }
     } catch (error) {
