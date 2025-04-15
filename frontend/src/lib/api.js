fc90d903087677a45d1b5425122423404ef1a72e
Adam Neves <adaamangelow@gmail.com>
2025-04-15 00:12:34 -0300

    fix: correct api

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:3333",
+  baseURL: "https://photos-api-434732873433.us-central1.run.app",
 });
 
 export default api;
